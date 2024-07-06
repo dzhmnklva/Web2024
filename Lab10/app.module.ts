37487619e457afa45ecf066f2c7e8908c219f2e6
dzhmnklva <151710664+dzhmnklva@users.noreply.github.com>
2024-07-06 20:22:50 +0500

    Add files via upload

---
+++ Lab10/app.module.ts
@@ -0,0 +1,25 @@
+// app.module.ts
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthInterceptor } from './auth.interceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent
+    // Add your components here
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
